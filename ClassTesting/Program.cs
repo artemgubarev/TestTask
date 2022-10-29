17891155138baca69b1abd3d92d5d05b56c93ecf
artemgubarev <64118632+artemgubarev@users.noreply.github.com>
2022-10-29 15:40:19 +0700

    factory

--- ClassTesting/Program.cs
+++ ClassTesting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,77 @@ namespace ClassTesting
 {
     class Program
     {
+        static List<Figure> init()
+        {
+            List<Figure> figures = new List<Figure>();
+
+            string path = "..\\..\\TestData.txt";
+            String string_line;
+
+            try
+            {
+                Creator creator;
+
+                StreamReader sr = new StreamReader(path);
+                string_line = sr.ReadLine();
+                while (string_line != null)
+                {
+                    string[] lines = string_line.Split();
+
+                    switch (lines[0])
+                    {
+                        case "point":
+                            creator = new PointCreator();
+                            Figure point = creator.Create(new double[] { Convert.ToDouble(lines[1]), Convert.ToDouble(lines[2]) });
+                            figures.Add(point);
+                            break;
+
+                        case "line":
+                            creator = new LineCreator();
+                            Figure line = creator.Create(new double[] { Convert.ToDouble(lines[1]), Convert.ToDouble(lines[2]),
+                            Convert.ToDouble(lines[3]), Convert.ToDouble(lines[4]) });
+                            figures.Add(line);
+                            break;
+
+                        case "circle":
+                            creator = new CircleCreator();
+                            Figure circle = creator.Create(new double[] { Convert.ToDouble(lines[1]), Convert.ToDouble(lines[2]), Convert.ToDouble(lines[3]) });
+                            figures.Add(circle);
+                            break;
+
+                        case "rect":
+                            creator = new RectCreator();
+                            Figure rect = creator.Create(new double[] { Convert.ToDouble(lines[1]), Convert.ToDouble(lines[2]),
+                                Convert.ToDouble(lines[3]), Convert.ToDouble(lines[4]) });
+                            figures.Add(rect);
+                            break;
+                    }
+
+                    string_line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return figures;
+        }
+
         static void Main(string[] args)
         {
-            Test.testPoint();
-            Test.testLine();
-            Test.testCircle();
-            Test.testRect();
+            List<Figure> figures = init();
+
+            foreach (var figure1 in figures)
+                foreach (var figure2 in figures)
+                {
+                    figure1.Draw();
+                    figure2.Draw();
+                    figure1.Intersect(figure2);
+                    figure2.Intersect(figure1);
+                    Console.WriteLine();
+                }
         }
     }
 }
---
+++ TestTask/Creator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    public interface Creator
+    {
+        Figure Create(double[] args);
+    }
+
+    public class PointCreator: Creator
+    {
+        public Figure Create(double[] args)
+        {
+            return new Point(args[0], args[1]);
+        }
+    }
+
+    public class LineCreator : Creator
+    {
+        public Figure Create(double[] args)
+        {
+            return new Line(new Point[2] { new Point(args[0], args[1]), new Point(args[2], args[3])});
+        }
+    }
+
+    public class CircleCreator : Creator
+    {
+        public Figure Create(double[] args)
+        {
+            return new Circle(new Point(args[0], args[1]), args[2]);
+        }
+    }
+
+    public class RectCreator : Creator
+    {
+        public Figure Create(double[] args)
+        {
+            return new Rect(new Point[2] { new Point(args[0], args[1]), new Point(args[2], args[3]) });
+        }
+    }
+
+}
